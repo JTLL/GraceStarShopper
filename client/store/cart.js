a5a557d54f7661caa4b50c7900510d06cfd6eb86
johnnycheng14 <johnnycheng14@gmail.com>
2018-07-30 13:09:56 -0400

    some work on attempting cart validation

--- client/store/cart.js
+++ client/store/cart.js
@@ -5,6 +5,7 @@ const REMOVE_STAR_FROM_CART = 'REMOVE_STAR_FROM_CART'
 const ADD_STAR_TO_CART = 'ADD_STAR_TO_CART'
 const RETURN_CART = 'RETURN_CART'
 const EMPTY_OUT_CART = 'EMPTY_OUT_CART'
+const CLEAN_CART = 'CLEAN_CART'
 
 const defaultCart = []
 
@@ -25,6 +26,11 @@ const returnCart = cart => ({
 
 const emptyOutCart = () => ({
   type: EMPTY_OUT_CART
+})
+
+const cleanCart = invalidItems => ({
+  type: CLEAN_CART,
+  invalidItems
 })
 
 export const addToCart = (starId, userId) => async dispatch => {
@@ -57,31 +63,71 @@ export const removeFromCart = (starId, userId = 0) => async dispatch => {
 
 export const fetchCart = (userId = 0) => async dispatch => {
   let cart = []
-  if (userId > 0) {
-    const res = await axios.get(`api/cart/${userId}`)
-    for (let i = 0; i < res.data.stars.length; i++) {
-      let star = await axios.get(`api/stars/${res.data.stars[i]}`)
-      cart.push(star.data)
-    }
-  } else {
-    if (localStorage.getItem('starCart')) {
-      let starCart = localStorage.getItem('starCart').split(',')
-      for (let i = 1; i < starCart.length; i++) {
-        let star = await axios.get(`api/stars/${starCart[i]}`)
+  try {
+    if (userId > 0) {
+      const res = await axios.get(`api/cart/${userId}`)
+      for (let i = 0; i < res.data.stars.length; i++) {
+        let star = await axios.get(`api/stars/${res.data.stars[i]}`)
         cart.push(star.data)
       }
+    } else {
+      if (localStorage.getItem('starCart')) {
+        let starCart = localStorage.getItem('starCart').split(',')
+        for (let i = 1; i < starCart.length; i++) {
+          let star = await axios.get(`api/stars/${starCart[i]}`)
+          cart.push(star.data)
+        }
+      }
     }
+    dispatch(returnCart(cart))
+  } catch (error) {
+    console.error(error)
   }
-  dispatch(returnCart(cart))
 }
 
 export const clearCart = (userId = 0) => async dispatch => {
-  if (userId) {
-    const newCart = await axios.put('/api/cart/remove', {userId})
-  } else {
-    localStorage.clear()
+  try {
+    if (userId) {
+      await axios.put('/api/cart/remove', {userId})
+    } else {
+      localStorage.clear()
+    }
+    dispatch(emptyOutCart())
+  } catch (error) {
+    console.error(error)
   }
-  dispatch(emptyOutCart())
+}
+
+const validateCart = (userId = 0, itemArray) => async dispatch => {
+  console.log("where am I?")
+  let userCart
+  try {
+    if (userId) {
+      let res = await axios.get(`/api/cart/${userId}`)
+      userCart = res.data.stars
+    } else {
+      userCart = localStorage.getItem('starCart').split(',')
+    }
+    console.log('usercart', userCart)
+    userCart.forEach(async starId => {
+      let res = await axios.get(`/api/stars/${starId}`)
+      let star = res.data
+      console.log("star",star)
+      if(star.owned){
+        itemArray.push(star.name)
+      }
+    })
+    dispatch(cleanCart(itemArray))
+  } catch (error) {
+    console.error(error)    
+  }
+}
+
+export const invalidCartItems = async (userId = 0) => {
+  let invalidItems = []
+  const test = await validateCart(userId, invalidItems)
+  console.log("invalidItems", invalidItems)
+  return invalidItems
 }
 
 export default function(state = defaultCart, action) {
@@ -99,6 +145,8 @@ export default function(state = defaultCart, action) {
       return action.cart
     case EMPTY_OUT_CART:
       return []
+    case CLEAN_CART:
+      return state.filter(star => !action.invalidItems.includes(star.id))
     default:
       return state
   }
