5b476080c27a11487a416fb88a17c50ee4ff0f39
hamadou diallo <hmaadou_diallo@yahoo.com>
2018-07-31 11:39:55 -0400

    test-spec backend route

---
+++ server/api/star.spec.js
@@ -0,0 +1,16 @@
+// const { expect } = require('chai')
+// const request = require('supertest')
+// const db = require('../db')
+// const app = require('../index')
+// const Star = db.model('stars')
+
+// describe.only('User routes', async () => {
+//   beforeEach(async () => {
+//     const stars = await Star.create({
+//       name: 'TidianeStar',
+//       magnitude: +0.13,
+//       price: 5000
+//     })
+
+//   })
+// })
