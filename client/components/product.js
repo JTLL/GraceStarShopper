ecbaef1f869a00e7932aa5307ede2ed42f003d82
Lawrence Lin <lawrence.y.lin@gmail.com>
2018-07-31 15:17:59 -0400

    fixed mult add

--- client/components/product.js
+++ client/components/product.js
@@ -1,48 +1,77 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {fetchCart} from '../store/cart'
 
-const Product = props => {
-  const {product, handleSubmit, handleRemove, location, userId, cart} = props
-  const cartReducer = (accumulator, currentValue) => {
-    return currentValue.id === product.id || accumulator
+class Product extends Component {
+  constructor(props) {
+    super(props)
+    this.props = props
   }
-  return (
-    <div className="ui card">
-      <div className="image">
-        <img src={product.image} style={{height: "290px", width: "290px"}}/>
-      </div>
-      <div className="content">
-        <p className="header">{product.name}</p>
-        <div className="description margin-bottom">
-          Magnitude: {product.magnitude} | Price: ${Number(
-            product.price
-          ).toLocaleString()}
+
+  componentDidMount() {
+    this.props.getCart(this.props.userId)
+  }
+
+  render() {
+    const {
+      product,
+      handleSubmit,
+      handleRemove,
+      location,
+      userId,
+      cart
+    } = this.props
+    const cartReducer = (accumulator, currentValue) => {
+      return currentValue.id === product.id || accumulator
+    }
+    console.log('cart from product page', this.props.cart)
+    return (
+      <div className="ui card">
+        <div className="image">
+          <img src={product.image} style={{height: '290px', width: '290px'}} />
         </div>
-        {location.pathname === '/cart' ? (
-          <button
-            className="ui button"
-            onClick={() => {
-              handleRemove(product.id, userId)
-            }}
-          >
-            Remove
-          </button>
-        ) : cart.reduce(cartReducer, false) ? (
-          <button className="ui button">Added</button>
-        ) : (
-          <div
-            className="ui vertical animated button"
-            tabIndex="0"
-            onClick={() => handleSubmit(product.id, userId)}
-          >
-            <div className="hidden content">Add</div>
-            <div className="visible content">
-              <i className="shop icon" />
-            </div>
+        <div className="content">
+          <p className="header">{product.name}</p>
+          <div className="description margin-bottom">
+            Magnitude: {product.magnitude} | Price: ${Number(
+              product.price
+            ).toLocaleString()}
           </div>
-        )}
+          {location.pathname === '/cart' ? (
+            <button
+              className="ui button"
+              onClick={() => {
+                handleRemove(product.id, userId)
+              }}
+            >
+              Remove
+            </button>
+          ) : cart.reduce(cartReducer, false) ? (
+            <button className="ui button">Added</button>
+          ) : (
+            <div
+              className="ui vertical animated button"
+              tabIndex="0"
+              onClick={() => handleSubmit(product.id, userId)}
+            >
+              <div className="hidden content">Add</div>
+              <div className="visible content">
+                <i className="shop icon" />
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
-export default Product
+const mapStateToProps = state => ({
+  cart: state.cart.validItems
+})
+
+const mapDispatchToProps = dispatch => ({
+  getCart: id => dispatch(fetchCart(id))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Product)
